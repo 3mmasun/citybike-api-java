9a9a6c8c531506a41938f6fe43d3467d52dc7577
Emma Sun <emma.sun@thoughtworks.com>
2019-12-12 10:24:06 +0800

    add data model for station and station bike

---
+++ src/main/java/com/tw/sg/dataguild/com/tw/sg/dataguild/model/Station.java
@@ -0,0 +1,10 @@
+package com.tw.sg.dataguild.com.tw.sg.dataguild.model;
+
+public class Station {
+    private String name;
+    private String id;
+    private float longitude;
+    private float latitude;
+    private String address;
+
+}
---
+++ src/main/java/com/tw/sg/dataguild/com/tw/sg/dataguild/model/StationBike.java
@@ -0,0 +1,13 @@
+package com.tw.sg.dataguild.com.tw.sg.dataguild.model;
+
+import java.util.Date;
+
+public class StationBike {
+    private short emptySlot;
+    private short renting;
+    private short returning;
+    private short freeBike;
+    private Date lastUpdated;
+    private String queryTimeStamp;
+}
+
